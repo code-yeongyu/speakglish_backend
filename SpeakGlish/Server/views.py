209476537e5047139b33fc1b9094000b544035dd
experse <3xperse@gmail.com>
2018-03-16 00:14:34 +0900

    이메일 로그인으로 변경

--- SpeakGlish/Server/views.py
+++ SpeakGlish/Server/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics, permissions
 from rest_framework.decorators import api_view
+import json
 
 class ArticleList(APIView) :
     def get(self, request, format=None):
